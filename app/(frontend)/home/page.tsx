25d78df321b4245e3a704dd4f161fd75e7951e73
shurov <shurov.0.00.0@gmail.com>
2025-07-28 11:13:37 +0600

    How it work section done

--- app/(frontend)/home/page.tsx
+++ app/(frontend)/home/page.tsx
@@ -5,7 +5,7 @@ import HowItWorks from './components/howitworks/howitworks'
 export default function HomePage() {
   return (
     <div className=" w-full ">
-      <div className="w-[1200px] mx-auto flex flex-col">
+      <div className="">
         <HeroSection />
         <HowItWorks />
       </div>
